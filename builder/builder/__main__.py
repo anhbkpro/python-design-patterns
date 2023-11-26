38154afe2ffa071c3363504d3ef65ccc65572177
Anh Lai <laingocanh1990@gmail.com>
2023-11-26 18:06:29 +0700

    what problems the builder pattern solved

--- builder/builder/__main__.py
+++ builder/builder/__main__.py
@@ -2,6 +2,12 @@ from director import Director
 from mycomputer_builder import MyComputerBuilder
 from budgetbox_builder import BudgetBoxBuilder
 
+# Problems solved:
+# - too many parameters in constructor
+# - exposing attributes to client
+# - encapsulated the attributes
+# - enforced assembly order
+# - separated assembly from the components
 computer_builder = Director(MyComputerBuilder())
 computer_builder.build_computer()
 computer = computer_builder.get_computer()
